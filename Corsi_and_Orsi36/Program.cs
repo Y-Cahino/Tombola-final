7b328951f78c0618ff8b58ab3fbc37388ad20cbc
Yan Nascimento <yanzebedeu@hotmail.com>
2023-01-12 17:45:11 +0100

    Aggiungere i file di progetto.

---
+++ Corsi_and_Orsi36/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Corsi_and_Orsi36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //declare
+            int x,y, g2=2;
+            Random r=new Random();
+            bool[] v=new bool[90];
+
+        }
+    }
+}
