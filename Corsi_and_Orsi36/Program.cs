5ef9f9deb405e2f11bbf2686eb958fe6b60031d2
Yan Nascimento <yanzebedeu@hotmail.com>
2023-01-12 23:05:25 +0100

    tombola

--- Corsi_and_Orsi36/Program.cs
+++ Corsi_and_Orsi36/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Corsi_and_Orsi36
 {
@@ -11,9 +11,31 @@ namespace Corsi_and_Orsi36
         static void Main(string[] args)
         {
             //declare
-            int x,y, g2=2;
+            int x,y,z=2;
             Random r=new Random();
-            bool[] v=new bool[90];
+            bool[] v = new bool[90];
+            int[,] c1 = new int[9, 3];
+            int[,] c2 = new int[9, 3];
+            //tabella
+            Console.WriteLine("Generazione tabella");
+            for(int i=0; i<9;i++)
+            {
+                x = 13;
+                for(int j=0; j<10;j++)
+                {
+                    Console.SetCursorPosition(x,z);
+                    Console.WriteLine("*");
+                    x += 2;
+                }
+                z++;
+            }
+            //estrazione
+            for(int i=0; i<90;i++)
+            {
+                
+            }
+            
+                
 
         }
     }
